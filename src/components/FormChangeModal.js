1696b401ecb654b33134b4de2980e3bdaa212b34
Andrey Artemenko <35538134+Andrew61-20@users.noreply.github.com>
2020-12-13 22:17:17 +0200

    Update FormChangeModal.js

--- src/components/FormChangeModal.js
+++ src/components/FormChangeModal.js
@@ -31,8 +31,9 @@ export default class FormChangeModal extends Component {
   handleSubmit = (act) => {
     const {data, update, closeModal } = this.props;
     closeModal();
-    axios.put(`https://my-json-server.typicode.com/Andrew61-20/DB/users/${act.id}`, {
-	  name: act.name,
+    //axios.put(`https://my-json-server.typicode.com/Andrew61-20/DB/users/${act.id}`, {
+     axios.put(`....../${act.id}`, {
+      name: act.name,
       phone: act.phone,
       image: act.image 
     }).then(response => {
